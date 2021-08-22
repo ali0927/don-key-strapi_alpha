a731fec613ca88366ff44e889dae713b73adf94e
ali0927 <kevinali0927@gmail.com>
2021-08-22 14:22:19 +0530

    Allow Everyone

--- config/middleware.js
+++ config/middleware.js
@@ -2,7 +2,7 @@ module.exports = {
   settings: {
     cors: {
       enabled: true,
-      origin: ["https://dev--don-keyfinance.netlify.app", "https://don-key.finance", "http://localhost:3000"],
+      origin: ["*"],
     },
   },
 };
