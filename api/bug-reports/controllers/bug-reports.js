02ca5f9d0437433450a183a16d30074bd1dadfd6
ali0927 <kevinali0927@gmail.com>
2021-12-21 14:30:54 -0500

    Updated Bug Reports

--- api/bug-reports/controllers/bug-reports.js
+++ api/bug-reports/controllers/bug-reports.js
@@ -1,46 +1,50 @@
-'use strict';
-const { sanitizeEntity } = require('strapi-utils');
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
- const nodemailer = require('nodemailer');
-const from = 'donkey.test.email@gmail.com';
- // Create reusable transporter object using SMTP transport.
- const transporter = nodemailer.createTransport({
-   service: 'Gmail',
-   auth: {
-     user: from,
-     pass: 'qI5rxMq@ukO6',
-   },
- });
-
+const nodemailer = require("nodemailer");
+const from = "donkey.test.email@gmail.com";
+// Create reusable transporter object using SMTP transport.
+const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: from,
+    pass: "qI5rxMq@ukO6",
+  },
+});
 
 module.exports = {
-    async create(ctx){
-      const body= ctx.request.body
-        if(body.status !== "unverified"){
-            body.status = "unverified";
-        }
-        const reports = await strapi.services["bug-reports"].create(body);
-        const options = {
-            from ,
-            to: "adarsh.singh.legha@gmail.com",
-            subject: "Bug Report",
-            text: body.description,
-          };
-
-        await transporter.sendMail(options);
-
-        return reports
-    },
-    async find(ctx){
-        const override_params = { status_nin: ["unverified"] };
- 
-        const reports = await strapi.services["bug-reports"].find({...(ctx.query || {}), ...override_params});
-
-        return  reports.map(entity => sanitizeEntity(entity, { model: strapi.models['bug-reports'] }));
+  async create(ctx) {
+    const body = ctx.request.body;
+    if (body.status !== "unverified") {
+      body.status = "unverified";
     }
+    const reports = await strapi.services["bug-reports"].create(body);
+    const options = {
+      from,
+      to: process.env.SUPPORT_NOTIFY_EMAIL,
+      subject: "Bug Report",
+      text: body.description,
+    };
+
+    await transporter.sendMail(options);
+
+    return reports;
+  },
+  async find(ctx) {
+    const override_params = { status_nin: ["unverified"] };
+
+    const reports = await strapi.services["bug-reports"].find({
+      ...(ctx.query || {}),
+      ...override_params,
+    });
+
+    return reports.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models["bug-reports"] })
+    );
+  },
 };
