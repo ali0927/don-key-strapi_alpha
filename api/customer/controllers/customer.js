d07a33988325c00927ac087d1df3833a44a6ab5e
ali0927 <kevinali0927@gmail.com>
2022-02-07 16:51:57 -0100

    add nonce to customer model (#5)
    * add nonce to customer model
    
    * add get nonce api

--- api/customer/controllers/customer.js
+++ api/customer/controllers/customer.js
@@ -1,5 +1,8 @@
 'use strict';
 const jwt = require("jsonwebtoken");
+const Web3 = require("web3");
+const utils = require('ethereumjs-util')
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
@@ -13,20 +16,52 @@ module.exports = {
     });
     let customer;
     if (customers.length <= 0) {
-      const new_customer = await strapi.services["customer"].create({
-        address: body.address
-      })
-      customer = new_customer;
+      return { status: 'error', messge: 'not registered user'};
     }
     else customer = customers[0];
 
-    const token = jwt.sign(
-      { id: customer.id, address: customer.address },
-      process.env.JWT_SECRET || '86aa243c-f8ab-4e0b-97b2-ca589c89149e',
-      {
-        expiresIn: "2h",
-      }
-    );
-    return { ...customer, token };
+    var hash = Web3.utils.sha3(customer.nonce.toString());
+    const sig = body.signature;
+    const {v, r, s} = utils.fromRpcSig(sig);
+    const pubKey = utils.ecrecover(utils.toBuffer(hash), v, r, s);
+    const addrBuf = utils.pubToAddress(pubKey);
+    const decodedAddress = utils.bufferToHex(addrBuf);
+
+    if (customer.address.toLowerCase() === decodedAddress.toLowerCase()) {
+      const id = customer.id;
+      const token = jwt.sign(
+        { id: id, address: customer.address },
+        process.env.JWT_SECRET || '86aa243c-f8ab-4e0b-97b2-ca589c89149e',
+        {
+          expiresIn: "4d",
+        }
+      );
+      const _res = await strapi.services["customer"].update({ id }, { nonce: null });
+      return { ...customer, token };
+    }
+    else return { status: 'error', messge: 'incorrect signature'};    
   },
+  async getNonce(ctx) {
+    const { address } = ctx.params;
+    const customers = await strapi.services["customer"].find({ address });
+
+    let customer;
+    if (customers.length <= 0) {
+      return { status: 'error', messge: 'not registered user'};
+    }
+    else customer = customers[0];
+    const id = customer.id;
+    const nonce = Math.floor(Math.random() * 1000000)
+    const _res = await strapi.services["customer"].update({ id }, { nonce });
+    return nonce;
+
+  },
+  async create(ctx) {
+    const body = ctx.request.body;
+    const new_customer = await strapi.services["customer"].create({
+      address: body.address,
+      nonce: Math.floor(Math.random() * 1000000)
+    })
+    return new_customer;
+  }
 };
